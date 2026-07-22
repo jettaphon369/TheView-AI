aa4f85d75a3f2b924827c1f9c743927a3c473ff6
jettaphon369 <jettaphon369@gmail.com>
2026-07-22 21:42:05 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// ใส่ Firebase Config ของโปรเจกต์พี่ที่นี่
+// โปรเจกต์นี้ใช้ Blaze Plan เพื่อรองรับ Cloud Storage; ตั้ง Budget Alert ไว้ใน Google Cloud Billing
+export const firebaseConfig = {
+  apiKey: "AIzaSyAB6aYZeQapkUJDkLwH2OK2A3XYLXvJE_0",
+  authDomain: "theview-4d389.firebaseapp.com",
+  projectId: "theview-4d389",
+  storageBucket: "theview-4d389.firebasestorage.app",
+  messagingSenderId: "146858365827",
+  appId: "1:146858365827:web:504f3333810f45a1981cb6"
+};
